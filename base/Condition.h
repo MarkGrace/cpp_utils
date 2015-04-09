b64c71361f09cb5bd057c9d21eb89001b320b8bb
mark <shark.wq@gmail.com>
2015-04-09 22:40:52 +0800

    xiaoxiugai

--- base/Condition.h
+++ base/Condition.h
@@ -5,22 +5,41 @@
  *      Author: mark
  */
 
-#ifndef THREADPOOL_CONDITION_H_
-#define THREADPOOL_CONDITION_H_
+#ifndef BASE_CONDITION_H_
+#define BASE_CONDITION_H_
 
 #include <pthread.h>
 
 class Condition
 {
-	Condition() { pthread_cond_init(&mCondition, NULL); }
-	~Condition() { pthread_cond_destroy(&mCondition); }
-	void wait(pthread_mutex_t *mutex) { pthread_cond_wait(&mCondition, mutex); }
-	void signal() { pthread_cond_signal(&mCondition); }
-	void broadcast() { pthread_cond_broadcast(&mCondition); }
+public:
+	Condition()
+	{
+		pthread_cond_init(&mCondition, NULL);
+	}
+
+	~Condition()
+	{
+		pthread_cond_destroy(&mCondition);
+	}
+
+	void wait(pthread_mutex_t *mutex)
+	{
+		pthread_cond_wait(&mCondition, mutex);
+	}
+
+	void signal()
+	{
+		pthread_cond_signal(&mCondition);
+	}
+
+	void broadcast()
+	{
+		pthread_cond_broadcast(&mCondition);
+	}
+
 private:
 	pthread_cond_t mCondition;
 };
 
-
-
-#endif /* THREADPOOL_CONDITION_H_ */
+#endif /* BASE_CONDITION_H_ */
